a321fc6aa40ce112b4313bac026fd31a6a3570e4
MnStan <mmaks97@gmail.com>
2022-11-08 18:14:31 +0100

    NetworkManager cleanup

--- Cepik/Managers/NetworkManager.swift
+++ Cepik/Managers/NetworkManager.swift
@@ -11,12 +11,7 @@ class NetworkManager {
     static let shared = NetworkManager()
     private let baseURL = "https://api.cepik.gov.pl/"
     let decoder = JSONDecoder()
-    
-    #warning("Change that")
-    private let restURL = "pojazdy?wojewodztwo=30&data-od=20120102&data-do=20130505&typ-daty=1&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&fields=marka&limit=200&page=1"
-    
-    // https://api.cepik.gov.pl/pojazdy?wojewodztwo=30&data-od=20000401&data-do=20020401&typ-daty=1&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=100&page=1
-    
+
     private init() {
         decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
